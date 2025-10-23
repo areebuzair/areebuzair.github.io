a0d585c9502828fe737aecbc1d432aed2abe7001
areeb <areeb@gmail.com>
2025-10-23 18:10:58 +0600

    Begins work on text sections

--- script.js
+++ script.js
@@ -6,4 +6,22 @@ window.addEventListener("load", () => {
         root.style.setProperty("--mouse-x", `${e.clientX}px`);
         root.style.setProperty("--mouse-y", `${e.clientY}px`);
     });
+
+    let sparkles = document.querySelectorAll(".sparkles");
+    let sparkleContainer = document.querySelector(".sparkly-word");
+    let i = 0;
+    for (let sparkle of sparkles) {
+        i++;
+        setTimeout(() => {
+            setInterval(() => {
+                let x = Math.round(Math.random() * 100);
+                let y = Math.round(Math.random() * 100);
+                sparkle.style.top = `${x}%`;
+                sparkle.style.left = `${y}%`;
+                sparkle.style.transform = `translate(${-y}%, ${-x}%)`;
+                sparkle.style.transformOrigin = `${-y + 50}% ${-x + 50}%`;
+                sparkleContainer.appendChild(sparkle)
+            }, 1000)
+        }, i * 300)
+    }
 })
